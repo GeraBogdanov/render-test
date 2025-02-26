7ef705349fe5145cd701bf1df60e62d205d54a17
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2025-02-26 19:29:20 +0300

    uibuild

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const generateId = () => {
   const maxId = notes.length > 0 
     ? Math.max(...notes.map((n) => Number(n.id))) 
     : 0;
-  return String(maxId + 1)
+  return String(maxId + 1);
 }
 
 app.post('/api/notes', (request, response) => {
